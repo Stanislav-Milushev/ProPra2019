db7995476bdd7d6c05baa34c58642f3b4d44de62
Yannick <yannick.lapp@student.uni-siegen.de>
2019-12-09 17:45:08 +0100

    Added JoinQuiz class

---
+++ QuizApp/src/main/java/propra/grpproj/quiz/SocketDataObjects/JoinQuiz.java
@@ -0,0 +1,21 @@
+package propra.grpproj.quiz.SocketDataObjects;
+
+import java.io.Serializable;
+
+public class JoinQuiz implements Serializable{
+	private String username;
+	private int quizID;
+	
+	public JoinQuiz(String username, int quizID) {
+		this.username = username;
+		this.quizID = quizID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public int getQuizID() {
+		return quizID;
+	}
+}
