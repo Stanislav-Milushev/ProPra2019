724a52ccc4642e515d1b45bd432411cbf2931b24
Marius <Marius.discher@t-online.de>
2019-12-09 14:47:36 +0100

    getList added

--- QuizApp/src/main/java/propra/grpproj/quiz/SocketDataObjects/PubList.java
+++ QuizApp/src/main/java/propra/grpproj/quiz/SocketDataObjects/PubList.java
@@ -5,4 +5,10 @@ import java.util.ArrayList;
 
 public class PubList implements Serializable{
 	public ArrayList<Pub> list = new ArrayList<Pub>();
+	
+	public ArrayList<Pub> getList () {
+		
+		ArrayList<Pub> plist = this.list;
+		return list;
+	}
 }
