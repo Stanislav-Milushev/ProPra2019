994250088abfda452dbae472d6e23dc51c7acb9e
Yannick <yannick.lapp@student.uni-siegen.de>
2019-12-11 10:32:29 +0100

    Moved question explanation in its own class

---
+++ QuizApp/src/main/java/propra/grpproj/quiz/SocketDataObjects/Explanation.java
@@ -0,0 +1,16 @@
+package propra.grpproj.quiz.SocketDataObjects;
+
+import java.io.Serializable;
+
+public class Explanation implements Serializable{
+	private String explanation;
+
+	public Explanation(String explanation) {
+		this.explanation = explanation;
+	}
+
+	public String getExplanation() {
+		return explanation;
+	}
+	
+}
--- QuizApp/src/main/java/propra/grpproj/quiz/SocketDataObjects/Question.java
+++ QuizApp/src/main/java/propra/grpproj/quiz/SocketDataObjects/Question.java
@@ -6,7 +6,6 @@ public class Question implements Serializable{
 	private int ID;
 	private String question;
 	private String[] answers;
-	private String explanation;
 	
 	public int getID() {
 		return ID;
@@ -19,15 +18,9 @@ public class Question implements Serializable{
 	public String[] getAnswers() {
 		return answers;
 	}
-	
-	public String getExplanation() {
-		return explanation;
-	}
-
-	public Question(int iD, String question, String[] answers, String explanation) {
+	public Question(int iD, String question, String[] answers) {
 		this.ID = iD;
 		this.question = question;
 		this.answers = answers;
-		this.explanation = explanation;
 	}
 }
