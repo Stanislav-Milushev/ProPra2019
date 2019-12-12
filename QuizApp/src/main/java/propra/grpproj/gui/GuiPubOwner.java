de6b2acc7e2f186b57f8e114b5c9839cd927893f
Marius <Marius.discher@t-online.de>
2019-12-12 13:39:16 +0100

    Gui Options header added

--- QuizApp/src/main/java/propra/grpproj/gui/GuiPubOwner.java
+++ QuizApp/src/main/java/propra/grpproj/gui/GuiPubOwner.java
@@ -25,7 +25,7 @@ public class GuiPubOwner {
 	/**
 	 * Launch the application.
 	 */
-	public static void main4(String[] args) {
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
