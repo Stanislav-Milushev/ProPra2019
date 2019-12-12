6a76302e91faf9fd3c44c088b085a0b8712a0515
Marius <Marius.discher@t-online.de>
2019-12-12 23:52:55 +0100

    ..

--- QuizApp/src/main/java/propra/grpproj/gui/GuiUserLogin.java
+++ QuizApp/src/main/java/propra/grpproj/gui/GuiUserLogin.java
@@ -281,6 +281,7 @@ public class GuiUserLogin {
 	 */
 	public void login_Return(UserType usertype) {
 		
+		System.out.println("Ich bin da ");
 		JFrame parent = new JFrame();
 		
 		switch (usertype) {
