be9d81d6f510172716ba11e3265188b9e196c4b2
Marius <Marius.discher@t-online.de>
2019-12-12 17:33:21 +0100

    ,

--- QuizApp/src/main/java/propra/grpproj/gui/GuiRegister.java
+++ QuizApp/src/main/java/propra/grpproj/gui/GuiRegister.java
@@ -11,7 +11,7 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.io.IOException; 
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
